44692da6945843beecb2522ef83b7cc0489d7566
asmaa-aljazzar <asma.jazar.319@gmail.com>
2026-09-22 06:54:32 +0300

    Finish Q1

---
+++ Exercises/Q1/expressions.js
@@ -0,0 +1,102 @@
+console.log("-9*3 => " + (-9 * 3));
+console.log("==========================");
+
+console.log('"value is" + 50 => ' + ("value is" + 50));
+console.log("==========================");
+
+console.log("17% 5 => " + (17 % 5));
+console.log("==========================");
+
+console.log("5% 17 => " + (5 % 17));
+console.log("==========================");
+
+console.log("5/10 => " + (5 / 10));
+console.log("==========================");
+
+console.log("(4 == 4) => " + (4 == 4));
+console.log("==========================");
+
+console.log("(4 != 5) => " + (4 != 5));
+console.log("==========================");
+
+console.log("(7 <= 8) => " + (7 <= 8));
+console.log("==========================");
+
+// the larges integer less than n.2, the biggest integer bigger than n.2
+console.log("Math.ceil(4.2) - Math.floor(4.2) => " + (Math.ceil(4.2) - Math.floor(4.2)));
+console.log("==========================");
+
+console.log("typeof(100) => " + typeof(100));
+console.log("==========================");
+
+
+console.log("typeof(73.9) => " + typeof(73.9));
+console.log("==========================");
+
+/*
+? NaN
+* Dividing zero by zero: 0 / 0
+* Taking the square root of a negative number: Math.sqrt(-1)
+* Invalid arithmetic operations: "hello" * 5
+* Because NaN represents the result of a failed numeric operation
+* within the number system, the language engine treats it as belonging to the Numeric type.
+*/
+console.log("typeof(NaN) => " + typeof(NaN));
+console.log("==========================");
+
+
+console.log('typeof("Water") => ' + typeof("Water"));
+console.log("==========================");
+
+
+console.log("typeof(false) => " + typeof(false));
+console.log("==========================");
+
+
+console.log("typeof(9 != 11) => " + typeof(9 != 11));
+console.log("==========================");
+
+
+console.log('"Orang" + "e" => ' + ("Orang" + "e"));
+console.log("==========================");
+
+
+console.log('"Orange" - "s" => ' + ("Orange" - "s"));
+console.log("==========================");
+
+
+console.log('"4" + "8" => ' + ("4" + "8"));
+console.log("==========================");
+
+
+console.log('"4" - "8" => ' + ("4" - "8"));
+console.log("==========================");
+
+console.log('"name" + 3 => ' + ("name" + 3));
+console.log("==========================");
+
+console.log('"name" - 3 => ' + ("name" - 3));
+console.log("==========================");
+
+console.log('82 * "word" => ' + (82 * "word"));
+console.log("==========================");
+
+console.log('1 + "hello" => ' + (1 + "hello"));
+console.log("==========================");
+
+console.log('"hello" + 1 => ' + ("hello" + 1));
+console.log("==========================");
+
+console.log("1 + true => " + (1 + true));
+console.log("==========================");
+
+console.log('"hello" + true => ' + ("hello" + true));
+console.log("==========================");
+
+console.log("typeof(Infinity) => " + typeof(Infinity));
+console.log("==========================");
+
+console.log("1 == '1' => " + (1 == '1'));
+console.log("==========================");
+
+console.log("1 === '1' => " + (1 === '1'));
