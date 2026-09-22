6d3201e4949f05d48dfc48c4a8250a9c5a7bfc4c
asmaa-aljazzar <asma.jazar.319@gmail.com>
2026-09-22 07:00:49 +0300

    Finish Q4

---
+++ Exercises/Q3/ascending.js
@@ -0,0 +1,8 @@
+const num1 = prompt ("Enter the first number");
+const num2 = prompt ("Enter the second number");
+
+if (num1 <= num2)
+    alert (num1 + " => " + num2);
+else
+    alert (num2 + " => " + num1);
+
---
+++ Exercises/Q4/ascending.js
@@ -0,0 +1,8 @@
+const num1 = prompt ("Enter the first number");
+const num2 = prompt ("Enter the second number");
+
+if (num1 >= num2)
+    alert (num1);
+else
+    alert (num2);
+
