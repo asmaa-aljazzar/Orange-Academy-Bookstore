2655c69c02a6915258eecdcd1023acfe3d4ecc99
asmaa-aljazzar <asma.jazar.319@gmail.com>
2026-09-22 07:17:39 +0300

    Finish Q5

---
+++ Exercises/Q5/sum.js
@@ -0,0 +1,17 @@
+// const num1 = prompt ("Enter the first number");
+// const num2 = prompt ("Enter the second number");
+
+// const sum = (num1 + num2);
+// The output of the promt input is string so the values was concatinated
+// alert (sum);
+
+/*
+*  The solution
+*/
+
+const num1 = Number (prompt ("Enter the first number"));
+const num2 = Number (prompt ("Enter the second number"));
+
+const sum = (num1 + num2);
+alert (sum);
+
