4dafd012e869cda1d183fdcae3b653e001eb1dae
asmaa-aljazzar <asma.jazar.319@gmail.com>
2026-09-23 06:15:34 +0300

    finish Ex2 Q5

---
+++ Exercises_2/Q5/numLoop.js
@@ -0,0 +1,11 @@
+let num;
+do {
+    num = prompt ("Enter a number between [ 0 and 100 ]");
+    if (num < 0 || num > 100)
+        alert ("Error: Invalid number");
+} while ((num < 0 || num > 100) && !isNaN(num))
+
+for (let i = 0; i <= 20; i += 3)
+    result += (i + " ");
+
+console.log(result);
