dc79743e4691140ce9f02044ff971613104a95e1
asmaa-aljazzar <asma.jazar.319@gmail.com>
2026-09-23 06:11:21 +0300

    fix Question 4

--- Exercises_2/Q4/numLoop.js
+++ Exercises_2/Q4/numLoop.js
@@ -1,4 +1,9 @@
-let result = "";
+let num;
+do {
+    num = prompt ("Enter a number between [ 0 and 100 ]");
+    if (num < 0 || num > 100)
+        alert ("Error: Invalid number");
+} while (num < 0 || num > 100)
 
 for (let i = 0; i <= 20; i += 3)
     result += (i + " ");
--- Exercises_2/Q5/numLoop.js
+++
@@ -1,6 +0,0 @@
-let result = "";
-
-for (let i = 0; i <= 20; i += 3)
-    result += (i + " ");
-
-console.log(result);
