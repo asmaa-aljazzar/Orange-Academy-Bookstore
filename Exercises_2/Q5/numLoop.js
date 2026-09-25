34962a8dd7a48aadfcf07b231f53d875d842d577
asmaa-aljazzar <asma.jazar.319@gmail.com>
2026-09-25 12:29:48 +0300

    Fix q4 & q5

--- Exercises_2/Q5/numLoop.js
+++ Exercises_2/Q5/numLoop.js
@@ -2,15 +2,14 @@ let input;
 let num;
 
 do {
-    input = prompt ("Enter a number between [ 0 and 100 ]");
-    if (input === null)
-    {
-        alert ("Error");
-        break ;
+    input = prompt("Enter a number between [ 0 and 100 ]");
+    if (input === null) {
+        alert("Error");
+        break;
     }
-    
-    num = Number (input);
 
-    if (input.trim == "" || num < 0 || num > 100)
-        alert ("Error: Invalid number");
-} while (input.trim == "" || num < 0 || num > 100)
+    num = Number(input);
+
+    if (input.trim == "" || isNaN(num) || num < 0 || num > 100)
+        alert("Error: Invalid number");
+} while (input.trim == "" || isNaN(num) || num < 0 || num > 100)
