47d96f7fb34ee5ebbb7a15fb89b3541bd7804e94
asmaa-aljazzar <asma.jazar.319@gmail.com>
2026-09-26 15:25:47 +0300

    Finish disable submit without valid data
    Print a success message when submit

--- Exercises_3/Basic/disabledSubmit/app.js
+++ Exercises_3/Basic/disabledSubmit/app.js
@@ -1,8 +1,10 @@
+const header = document.querySelector ("h1");
 const form = document.querySelector("form");
 const unameInput = document.getElementById("uname");
 const passInput = document.getElementById("pass");
 const confPassInput = document.getElementById("confPass");
 const button = document.querySelector("button");
+button.disabled = true;
 
 const unameLabel = document.createElement("label");
 unameLabel.textContent = "Username:";
@@ -20,56 +22,62 @@ confPassLabel.setAttribute("for", "confPass");;
 confPassInput.parentNode.insertBefore(confPassLabel, confPassInput);
 
 const isValidInput = (value) => {
-    if (!value || value.trim () === "")
+    if (!value || value.trim() === "")
         return false;
     return true;
 }
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    const uname = unameInput.value;
-    const password = passInput.value;
-    const confPassword = confPassInput.value;
-
-    const validUsername = isValidInput (uname); 
-    const validPass = isValidInput (password); 
-    const validConfPass = isValidInput (confPassword); 
-
-    if (!validUsername || !validPass || !validConfPass)
-    {
-        button.disabled = true;
-        return ;
-    }
-
-    if (notMatchMsg(password, confPassword))
-        return;
-    button.disabled = false;
-    console.log(`Form Submited: ${uname} ${password} ${confPassword}`);
-    form.reset();
-});
-
-
-//? Ex3: Matching password
-const notMatchMsg = (password, confPassword) => {
+const handlePasswordMismatch = (password, confPassword) => {
     const existError = confPassInput.nextElementSibling;
     const isErrorPresent = existError && existError.classList.contains("err-msg");
 
     if (password !== confPassword) {
-        if (!isErrorPresent)
-        {
-            const errMsg = document.createElement ("span");
+        if (!isErrorPresent) {
+            const errMsg = document.createElement("span");
             errMsg.textContent = "Passwords do not match";
             errMsg.style.color = "red";
-            errMsg.classList.add ("err-msg");
-            confPassInput.parentNode.insertBefore (errMsg, existError);
+            errMsg.classList.add("err-msg");
+            confPassInput.parentNode.insertBefore(errMsg, existError);
         }
-            return true;
+        return true;
     }
     else
         if (isErrorPresent)
-            existError.remove ();
-        return false;
+            existError.remove();
+    return false;
 }
+
+const validateForm = () => {
+    const uname = unameInput.value;
+    const password = passInput.value;
+    const confPassword = confPassInput.value;
+
+    const isValidUsername = isValidInput(uname);
+    const isValidPass = isValidInput(password);
+    const isValidConfPass = isValidInput(confPassword);
+    const isPassMismatch = handlePasswordMismatch (password, confPassword);
+
+    const isFormValid = isValidUsername && isValidPass && isValidConfPass && !isPassMismatch;
+    
+    button.disabled = !isFormValid;
+}
+
+form.addEventListener("input", validateForm);
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const successMsg = document.createElement ("span");
+    successMsg.textContent = "Form Submited Successfully!";
+    successMsg.style.color = "#00c24e";
+    header.parentNode.insertBefore (successMsg, form);
+
+    form.reset();
+    
+    button.disabled = true;
+});
+
+
 
 
