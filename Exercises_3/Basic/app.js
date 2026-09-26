22626a8e1865b6f30c21b2a84269e81d75ea02b5
asmaa-aljazzar <asma.jazar.319@gmail.com>
2026-09-26 10:58:42 +0300

    Finish Bascic Ex2 add Required fields handling

--- Exercises_3/Basic/app.js
+++ Exercises_3/Basic/app.js
@@ -1,27 +1,63 @@
-const form = document.querySelector ("form");
-const unameInput = document.getElementById ("uname");
-const passInput = document.getElementById ("pass");
-const confPassInput = document.getElementById ("confPass");
-const button = document.querySelector ("button");
+const form = document.querySelector("form");
+const unameInput = document.getElementById("uname");
+const passInput = document.getElementById("pass");
+const confPassInput = document.getElementById("confPass");
+const button = document.querySelector("button");
 
 //? Ex1: Add labels
 //setAttribute => (attribute name, attribute value)
-const unameLabel = document.createElement ("label");
+const unameLabel = document.createElement("label");
 unameLabel.textContent = "Username:";
-unameLabel.setAttribute ("for", "uname");;
+unameLabel.setAttribute("for", "uname");;
 // parentNode => Find the parent node that hold that input
 //insertBefore => Will go to parent search for the 2nd att and add the 1st att before it.
-unameInput.parentNode.insertBefore (unameLabel, unameInput);
+unameInput.parentNode.insertBefore(unameLabel, unameInput);
 
-const passLabel = document.createElement ("label");
+const passLabel = document.createElement("label");
 passLabel.textContent = "Password:";
-passLabel.setAttribute ("for", "uname");;
-passInput.parentNode.insertBefore (passLabel, passInput);
+passLabel.setAttribute("for", "uname");;
+passInput.parentNode.insertBefore(passLabel, passInput);
 
-const confPassLabel = document.createElement ("label");
+const confPassLabel = document.createElement("label");
 confPassLabel.textContent = "ConfirmPassword:";
-confPassLabel.setAttribute ("for", "uname");;
-confPassInput.parentNode.insertBefore (confPassLabel, confPassInput);
+confPassLabel.setAttribute("for", "uname");;
+confPassInput.parentNode.insertBefore(confPassLabel, confPassInput);
 
+const requiredMsg = (input, value) => {
+    let existingError = input.nextSibling;
+    const isErrorPresent = existingError && existingError?.classList?.contains("err-msg");
 
+    if (!value || value.trim() == "") {
+        if (!isErrorPresent) {
+            const errMsg = document.createElement("span");
+            errMsg.textContent = "Required";
+            errMsg.style.color = "red";
+            errMsg.classList.add ("err-msg");
+            // nextSibling means go to the next sibling and put it before it
+            input.parentNode.insertBefore(errMsg, input.nextSibling);
+        }
+        return true;
+    }
+    else
+        if (isErrorPresent)
+            existingError.remove();
+    return false;
+}
 
+//? Ex2: Required Input
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const uname = unameInput.value;
+    const password = passInput.value;
+    const confPassword = confPassInput.value;
+
+    const unameErr = requiredMsg(unameInput, uname);
+    const passErr = requiredMsg(passInput, password);
+    const confPassErr = requiredMsg(confPassInput, confPassword);
+    
+    if (unameErr || passErr || confPassErr)
+        return ;
+
+    form.reset();
+})
